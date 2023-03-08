f765d5f6fc55c6279ee76716d2101c8dce532efa
KlaudijaDemchyk <fkst@i.ua>
2023-03-08 20:53:56 +0100

    Init

--- js/domOperations.js
+++ js/domOperations.js
@@ -4,28 +4,28 @@ import {ResultInDomParalelograma} from "./libraryFormula.js";
 import {ResultInDomRomba} from "./libraryFormula.js";
 document.getElementById('b1').onclick =showResultInDomKvadrat;
 function showResultInDomKvadrat(){
-    var a = document.getElementById('dlinaStoroniKvadrata').value;
-    var result = ResultInDomKvadrat(a);
+    const a = document.getElementById('dlinaStoroniKvadrata').value;
+    const result = ResultInDomKvadrat(a);
     document.getElementById ('ploschadKvadrata').textContent = result;
 }
 document.getElementById('b2').onclick =showResultInDomPriamougolnika;
 function showResultInDomPriamougolnika(){
-    var a = document.getElementById('pervayaStoronaPriamougolnika').value;
-    var b = document.getElementById('drugayaStoronaPriamougolnika').value;
-    var result = ResultInDomPriamougolnika(a,b);
+    const a = document.getElementById('pervayaStoronaPriamougolnika').value;
+    const b = document.getElementById('drugayaStoronaPriamougolnika').value;
+    const result = ResultInDomPriamougolnika(a,b);
     document.getElementById ('ploschadPriamougolnika').textContent = result;
 }
 document.getElementById('b3').onclick =showResultInDomParalelograma;
 function showResultInDomParalelograma(){
-    var a = document.getElementById('dlinaStoronyParalelograma').value;
-    var h = document.getElementById ('dlinaVisotyParalelograma').value;
-    var result = ResultInDomParalelograma(a,h);
+    const a = document.getElementById('dlinaStoronyParalelograma').value;
+    const h = document.getElementById ('dlinaVisotyParalelograma').value;
+    const result = ResultInDomParalelograma(a,h);
     document.getElementById ('ploschadParalelograma').textContent = result;
 }
 document.getElementById('b4').onclick =showResultInDomRomba;
 function showResultInDomRomba(){
-    var a = document.getElementById('dlinaStoronyRomba').value;
-    var h = document.getElementById ('dlinaVisotyRomba').value;
-    var result = ResultInDomRomba(a,h);
+    const a = document.getElementById('dlinaStoronyRomba').value;
+    const h = document.getElementById ('dlinaVisotyRomba').value;
+    const result = ResultInDomRomba(a,h);
     document.getElementById ('ploschadRomba').textContent = result;
 }
